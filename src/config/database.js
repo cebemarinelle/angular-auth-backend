16b07e8e57dc4773ddcfb9dee745cacf1c5e5760
cebemarinelle <marinellecebeg@gmail.com>
2026-05-20 03:57:17 +0800

    Fix: Use DB_URL for cloud MySQL connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+// Use DB_URL from environment variables (Aiven cloud MySQL)
+const pool = mysql.createPool(process.env.DB_URL);
 
 module.exports = pool.promise();
